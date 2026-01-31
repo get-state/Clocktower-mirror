0c3e29033f03f068e6fbcd015ab18fefa25c198e
Mazin Al Haddad <mazin@getstate.dev>
2026-01-31 03:10:11 +0000

    Bug: Fix cornelis package definition.

--- clocktower/packages/cornelis.scm
+++ clocktower/packages/cornelis.scm
@@ -12,48 +12,49 @@
   #:use-module (guix packages))
 
 (define-public agda-cornelis-2.7.1
-  ;; Version should always be matched to the same agda release. Match guix upstream. (package
-  (name "cornelis")
-  (version "2.7.1")
-  (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/agda/cornelis")
-                  (commit (string-append "v" version))))
-            (file-name (git-file-name name version))
-            (sha256 (base32
-                     "1c92mjmkqh2xyjav6sjk2ba068mi2s4j8js4rfij8f9711x00pw7"))))
-  (build-system haskell-build-system)
-  (properties '((upstream-name . "cornelis")))
-  (inputs (list ghc-quickcheck
-                ghc-aeson
-                ghc-async
-                ghc-diff-loc
-                ghc-fingertree
-                ghc-generic-lens
-                ghc-hspec
-                ghc-hspec-discover
-                ghc-lens
-                ghc-levenshtein
-                ghc-megaparsec
-                ghc-nvim-hs
-                ghc-nvim-hs-contrib
-                ghc-prettyprinter
-                ghc-random
-                ghc-resourcet
-                ghc-unagi-chan
-                ghc-unliftio-core
-                ghc-vector))
-  (native-inputs (list ghc-temporary ghc-hspec ghc-hspec-discover))
-  (home-page "https://github.com/agda/cornelis")
-  (synopsis "Agda-mode, but for neovim")
-  (description
-   "It supports highlighting, goal listing, type-context,
+  ;; Version should always be matched to the same agda release. Match guix upstream.
+  (package
+    (name "cornelis")
+    (version "2.7.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/agda/cornelis")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1c92mjmkqh2xyjav6sjk2ba068mi2s4j8js4rfij8f9711x00pw7"))))
+    (build-system haskell-build-system)
+    (properties '((upstream-name . "cornelis")))
+    (inputs (list ghc-quickcheck
+                  ghc-aeson
+                  ghc-async
+                  ghc-diff-loc
+                  ghc-fingertree
+                  ghc-generic-lens
+                  ghc-hspec
+                  ghc-hspec-discover
+                  ghc-lens
+                  ghc-levenshtein
+                  ghc-megaparsec
+                  ghc-nvim-hs
+                  ghc-nvim-hs-contrib
+                  ghc-prettyprinter
+                  ghc-random
+                  ghc-resourcet
+                  ghc-unagi-chan
+                  ghc-unliftio-core
+                  ghc-vector))
+    (native-inputs (list ghc-temporary ghc-hspec ghc-hspec-discover))
+    (home-page "https://github.com/agda/cornelis")
+    (synopsis "Agda-mode, but for neovim")
+    (description
+     "It supports highlighting, goal listing, type-context,
 refinement, auto, solving, case splitting, go-to definition, normalization,
 and helper functions.  These are exposed via vim commands. Most commands
 have an equivalent in agda-mode.")
-  (license license:bsd-3))
-unbalanced
+    (license license:bsd-3)))
 
 (define-public agda-cornelis-2.8.0
   (package
